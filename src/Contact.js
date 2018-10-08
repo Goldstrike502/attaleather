0961dc7a0e5d7e12c431a7217c8bda748c264585
Goldstrike <sebastiaanvandijkhuizen@hotmail.com>
2018-10-08 16:37:27 +0200

    contact

--- src/Contact.js
+++ src/Contact.js
@@ -8,7 +8,7 @@ export class Contact extends React.Component {
             <div>
                 <h1><span>Contact</span></h1>
             <div className="contact">
-                <form method="POST" action="https://formspree.io/sebastiaanvandijkhuizen@hotmail.com">
+                <form method="POST" action="https://formspree.io/info@attaleather.nl">
                     <input type="text" name="naam" placeholder="Uw naam" />
                     <input type="email" name="email" placeholder="Uw email adres" />
                     <textarea name="message" placeholder="Uw bericht" rows="15"></textarea>
